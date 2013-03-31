3d7eda3318efaf793ccfa6be158c12e53e74fa90
Sho Kusano <rosylilly@aduca.org>
2013-04-01 05:57:36 +0900

    Init

---
+++ lib/katakuchi.rb
@@ -0,0 +1,5 @@
+require "katakuchi/version"
+
+module Katakuchi
+  # Your code goes here...
+end
---
+++ spec/katakuchi_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Katakuchi do
+  it 'should have a version number' do
+    Katakuchi::VERSION.should_not be_nil
+  end
+
+  it 'should do something useful' do
+    false.should be_true
+  end
+end
